9c03735b013f848e13628b1bfe1681bc7704d518
bihut <angelrzafra@gmail.com>
2021-06-15 14:39:51 +0200

    commit

--- app/public/routes.py
+++ app/public/routes.py
@@ -5,7 +5,9 @@ from ..Tokens.model import Token
 from ..baby.model import Baby
 from ..event.model import Event
 from ..user.model import User
-
+from flask import Flask, render_template, Response
+import cv2
+import depthai as dai
 
 @public_bp.route('/username/<string:username>',methods=['GET'])
 def getUsername(username):
@@ -129,6 +131,64 @@ def createNewEvent():
                     return {'message': 'Error creating event'}, 401
         except:
            return {'message': 'Error creating event (data missed or already registered)'}, 401
+
+
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        # Create pipeline
+        pipeline = dai.Pipeline()
+
+        # Define source and output
+        camRgb = pipeline.createColorCamera()
+        xoutVideo = pipeline.createXLinkOut()
+
+        xoutVideo.setStreamName("video")
+
+        # Properties
+        camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+        camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        camRgb.setVideoSize(1280, 720)
+        ve2 = pipeline.createVideoEncoder()
+        ve2.setDefaultProfilePreset(1280, 720, 30, dai.VideoEncoderProperties.Profile.MJPEG)
+        camRgb.video.link(ve2.input)
+
+        #camRgb.setVideoSize(640,400)
+
+        xoutVideo.input.setBlocking(False)
+        xoutVideo.input.setQueueSize(1)
+
+        # Linking
+        camRgb.video.link(xoutVideo.input)
+        # Connect to device and start pipeline
+        qRgb = None
+        inRgb = None
+        frame = None
+        with dai.Device(pipeline) as device:
+            # Output queue will be used to get the rgb frames from the output defined above
+            qRgb = device.getOutputQueue(name="video", maxSize=1, blocking=False)
+
+            while True:
+                inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+                frame = inRgb.getCvFrame() # read the camera frame
+
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@public_bp.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@public_bp.route('/oak')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
 '''
 @public_bp.route('/')
 def index():
