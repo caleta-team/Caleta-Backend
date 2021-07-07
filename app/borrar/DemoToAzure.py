b355532b4cec109f040e2c5500797c78d61874d3
bihut <angelrzafra@gmail.com>
2021-07-07 13:38:05 +0200

    commit

--- app/borrar/DemoToAzure.py
+++ app/borrar/DemoToAzure.py
@@ -1,9 +1,9 @@
 from paho.mqtt import client as mqtt
 import ssl
 
-path_to_root_cert = "<local path to digicert.cer file>"
+path_to_root_cert = "/home/bihut/dev/CaletaTeam/Caleta-Backend-v2/app/borrar/certs.c"
 device_id = "pi-caleta-1"
-sas_token = "<generated SAS token>"
+sas_token = "HostName=iot-hub-caleta.azure-devices.net;DeviceId=pi-caleta-1;SharedAccessSignature=SharedAccessSignature sr=iot-hub-caleta.azure-devices.net%2Fdevices%2Fpi-caleta-1&sig=nXq60zirDFV8yDPU5cxYJCbwgm%2BvXqTsQ7KE%2Fsnn8kk%3D&se=1625655667"
 iot_hub_name = "iot-hub-caleta"
 
 
@@ -28,9 +28,13 @@ client.on_publish = on_publish
 client.username_pw_set(username=iot_hub_name+".azure-devices.net/" +
                        device_id + "/?api-version=2018-06-30", password=sas_token)
 
-client.tls_set(ca_certs=path_to_root_cert, certfile=None, keyfile=None,
+cert_file = "/home/bihut/dev/CaletaTeam/certificates/python-device.pem"
+key_file = "/home/bihut/dev/CaletaTeam/certificates/python-device.key.pem"
+client.tls_set(ca_certs=path_to_root_cert, certfile=cert_file, keyfile=key_file,
                cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
-client.tls_insecure_set(False)
+#client.tls_set("/home/bihut/dev/CaletaTeam/certificates/python-device.pem") # use builtin cert on Raspbian
+
+#client.tls_insecure_set(False)
 
 client.connect(iot_hub_name+".azure-devices.net", port=8883)
 
